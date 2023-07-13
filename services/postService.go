596d63bcce84c8340934e2ad395da0570c8e7e37
marcbudd <marcbuddemeier@gmail.com>
2023-07-13 07:44:23 +0200

    Debug feed

--- services/postService.go
+++ services/postService.go
@@ -154,8 +154,8 @@ func GetPostsForCurrentUser(currentUserID uint, limit int, page int) ([]*models.
 	result := db.
 		Joins("JOIN follows ON follows.user_followed_id = posts.user_id").
 		Where("follows.user_following_id = ? OR posts.user_id = ?", currentUserID, currentUserID).
-		Order("created_at DESC").
 		Distinct("posts.id").
+		Order("posts.created_at DESC").
 		Offset(offset).
 		Limit(limit).
 		Find(&posts)
